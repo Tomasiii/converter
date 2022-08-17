aff1af2d3d8b40166bbe7ec65a5280fef39ec002
Tomasiii <nsa90202@gmail.com>
2022-08-17 17:14:29 +0300

    fix:move api key to .env

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -4,7 +4,8 @@ export const axi = axios.create({
   baseURL: "https://api.apilayer.com/exchangerates_data/",
   headers: {
     "Content-Type": "application/json",
-    apikey: "nmurQGxFXGWOsTbXMrhTJyZ7IHi3piId",
+    apikey: String(process.env.API_KEY),
+
     // apikey: "3i87yrBKXJTRQ5vg1561Rz4FhbbYX0Sp",
     // apikey: "SNleukyUmI8lfR5vcpQKKgA9NHMjceRN"
   },
